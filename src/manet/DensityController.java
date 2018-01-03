3cbd697b2e15dbd768ab9499b0befa0459d031d3
AINAS TACAARINAS <3001048@ppti-14-508-06.ufr-info-p6.jussieu.fr>
2018-01-03 18:15:53 +0100

    03/01

--- src/manet/DensityController.java
+++ src/manet/DensityController.java
@@ -32,6 +32,9 @@ public class DensityController implements Control{
 		System.out.println("----------- AverageDensity "+getAverageDensity());
 		System.out.println("----------- AverageStandardDeviation "+getAverageStandardDeviation());
 		System.out.println("----------- DensityStandardDeviation "+getDensityStandardDeviation());
+		System.out.println("----------- D "+getAverageDensity());
+		//System.out.println("----------- E/D "+getStandardDeviation()/getAverageDensity());
+		System.out.println("----------- ED/D "+getDensityStandardDeviation()/getAverageDensity());
 		return false;
 	}
 
